3f679a2847c2caa3c719e38867d144bbabceb7cf
whdms705 <whdms705@nate.com>
2017-07-19 23:41:06 +0900

    click  관련 테스트 케이스 클래스 추가

---
+++ src/main/java/org/gradle/selenium/functions/click/ClickFuctionCase.java
@@ -0,0 +1,15 @@
+package org.gradle.selenium.functions.click;
+
+import org.apache.log4j.Logger;
+import org.gradle.selenium.FuctionTestCase;
+import org.openqa.selenium.WebDriver;
+
+public class ClickFuctionCase implements FuctionTestCase {
+	private final Logger log = Logger.getLogger(ClickFuctionCase.class);
+	
+	@Override
+	public void callme(WebDriver driver,String url){
+		
+	}
+
+}
